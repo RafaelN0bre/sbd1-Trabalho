e090471c70c62b352c29776c67b264fdf9d97e82
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2023-02-05 20:19:31 -0300

    sobre outras etapas da entrega

---
+++ 3 - Entrega/TF3_Apaga_RafaelNobre_200026488.sql
@@ -0,0 +1,42 @@
+--------  << TF_TEMA3_ACAI >>  ----------
+--
+--                SCRIPT DE REMOCAO (DDL)
+--
+-- Data Criacao ...........: 25/01/2023
+-- Autor(es) ..............: Rafael de Medeiros Nobre 
+--                           Rodrigo Edmar Wright Dos Santos
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: TF_TEMA3_ACAI
+--
+-- Ultimas Alteracoes
+-- 
+-- PROJETO => 01 Base de Dados
+--         => 23 Tabelas
+--
+-- ---------------------------------------------------------
+
+USE TF_TEMA3_ACAI;
+
+DROP TABLE fornece;
+DROP TABLE contem;
+DROP TABLE exerce;
+DROP TABLE trabalha;
+DROP TABLE feitoCom;
+DROP TABLE telefone;
+DROP TABLE ferias;
+DROP TABLE MANUTENCAO;
+DROP TABLE TIPOMANUTENCAO;
+DROP TABLE ABASTECIMENTO;
+DROP TABLE PEDIDOFORNECEDOR;
+DROP TABLE CLIENTE;
+DROP TABLE PAGAMENTOFUNCIONARIO;
+DROP TABLE FORNECEDOR;
+DROP TABLE VENDA;
+DROP TABLE TIPOVENDA;
+DROP TABLE FORMAPAGAMENTO;
+DROP TABLE ATIVIDADE;
+DROP TABLE OPCAOCARDAPIO;
+DROP TABLE LOJA;
+DROP TABLE FUNCIONARIO;
+DROP TABLE ENDERECO;
+DROP TABLE PRODUTO;
